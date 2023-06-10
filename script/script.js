7af7c5f99d6766a8f175e5436934667c9e264786
mahdi <ramezani86mraz@gmail.com>
2023-06-11 01:39:37 +0330

    7

--- script/script.js
+++ script/script.js
@@ -1,3 +1,8 @@
 const $ = document;
 const getId = id => $.getElementById(id)
 const getClass = classE => $.getElementById(classE)
+
+// to comput distance between transparent div and windows top in px
+const transparentTopHandler = () => getId('transparent').style.top = (parseInt(getComputedStyle(getId('background')).getPropertyValue('height')) -50) + 'px';
+window.addEventListener('load', transparentTopHandler)
+window.addEventListener('resize', transparentTopHandler)
