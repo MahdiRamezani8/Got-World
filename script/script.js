a289595b6d72a1529e2350fc1a034d9bcd4e2b60
mahdi <ramezani86mraz@gmail.com>
2023-06-15 13:33:30 +0330

    some changes commited, i'll complete changes afternoon or tomorrow

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
-const $ = document;
-const getId = id => $.getElementById(id)
-const getClass = classE => $.getElementById(classE)
+// const $ = document;
+// const getId = id => $.getElementById(id)
+// const getClass = classE => $.getElementById(classE)
 
-// to comput distance between transparent div and windows top in px
-const transparentTopHandler = () => getId('transparent').style.top = (parseInt(getComputedStyle(getId('background')).getPropertyValue('height')) -50) + 'px';
-window.addEventListener('load', transparentTopHandler)
-window.addEventListener('resize', transparentTopHandler)
+// // to comput distance between transparent div and windows top in px
+// const transparentTopHandler = () => getId('transparent').style.top = (parseInt(getComputedStyle(getId('background')).getPropertyValue('height')) -50) + 'px';
+// window.addEventListener('load', transparentTopHandler)
+// window.addEventListener('resize', transparentTopHandler)
