791bba24cabab180b4cdd502ed6f4c998849f784
mahdi <ramezani86mraz@gmail.com>
2023-06-16 12:38:48 +0330

    landing header completed - full responsive

--- script/script.js
+++ script/script.js
@@ -1,8 +1,16 @@
-// const $ = document;
-// const getId = id => $.getElementById(id)
-// const getClass = classE => $.getElementById(classE)
+const $ = document;
+const getId = id => $.getElementById(id)
+const getClass = classE => $.getElementById(classE)
 
 // // to comput distance between transparent div and windows top in px
 // const transparentTopHandler = () => getId('transparent').style.top = (parseInt(getComputedStyle(getId('background')).getPropertyValue('height')) -50) + 'px';
 // window.addEventListener('load', transparentTopHandler)
 // window.addEventListener('resize', transparentTopHandler)
+const searchInput = getId('search-input')
+window.addEventListener('load', searchInputPlaceholder)
+window.addEventListener('resize', searchInputPlaceholder)
+function searchInputPlaceholder(){
+    if (window.innerWidth < 375) {
+        searchInput.placeholder = "درباره چی میخوای بدونی؟"
+    }
+}
