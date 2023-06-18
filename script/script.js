3b035b62d96d4cf0cc27335250ab248c61911912
mahdi <ramezani86mraz@gmail.com>
2023-06-18 15:48:14 +0330

    john snow and some bugs are fixed

--- script/script.js
+++ script/script.js
@@ -10,24 +10,3 @@ function searchInputPlaceholder(){
         searchInput.placeholder = "درباره چی میخوای بدونی؟"
     }
 }
-
-function softScroll (pageNick) {
-    $('.' + pageNick + ' a[href*="#"]')
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function (event) {
-        if (
-            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                event.preventDefault();
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 500, function () {});
-            }
-        }
-    });
-};
