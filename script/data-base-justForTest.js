1fdc1150be02dce6aa901db31f9cdded25f0e718
mahdi <ramezani86mraz@gmail.com>
2023-06-19 23:41:39 +0330

    main page in done, there is some bugs and we'll hunt and fix them, i'm tired but it's good because i'm not an animal :)

--- script/data-base-justForTest.js
+++ script/data-base-justForTest.js
@@ -1,51 +1,51 @@
 const characters = [{
-    id: 1,
-    name: "john snow",
-    family: "stark",
-    imgAddress: "image/characters/john-snow.png"
-},
-{
-    id: 2,
-    name: "tyrion lanister",
-    family: "lanister",
-    imgAddress: "image/characters/tyrion-lannister.png"
-},
-{
-    id: 3,
-    name: "daenerys targaryen",
-    family: "targaryen",
-    imgAddress: "image/characters/daenerys-targaryen.png"
-},
-{
-    id: 4,
-    name: "jaime lanister",
-    family: "lanister",
-    imgAddress: "image/characters/jaime-lanister.png"
-},
-{
-    id: 5,
-    name: "arya stark",
-    family: "stark",
-    imgAddress: "image/characters/arya-stark.png"
-},
-{
-    id: 6,
-    name: "cersie lanister",
-    family: "lanister",
-    imgAddress: "image/characters/cersei-lanister.png"
-},
-{
-    id: 7,
-    name: "robb stark",
-    family: "stark",
-    imgAddress: "image/characters/robb-stark.png"
-},
-{
-    id: 8,
-    name: "sansa stark",
-    family: "stark",
-    imgAddress: "image/characters/sansa-stark.png"
-}
+        id: 1,
+        name: "john snow",
+        family: "stark",
+        imgAddress: "image/characters/john-snow.png"
+    },
+    {
+        id: 2,
+        name: "tyrion lanister",
+        family: "lanister",
+        imgAddress: "image/characters/tyrion-lannister.png"
+    },
+    {
+        id: 3,
+        name: "daenerys",
+        family: "targaryen",
+        imgAddress: "image/characters/daenerys-targaryen.png"
+    },
+    {
+        id: 4,
+        name: "jaime lanister",
+        family: "lanister",
+        imgAddress: "image/characters/jaime-lanister.png"
+    },
+    {
+        id: 5,
+        name: "arya stark",
+        family: "stark",
+        imgAddress: "image/characters/arya-stark.png"
+    },
+    {
+        id: 6,
+        name: "cersie lanister",
+        family: "lanister",
+        imgAddress: "image/characters/cersei-lanister.png"
+    },
+    {
+        id: 7,
+        name: "robb stark",
+        family: "stark",
+        imgAddress: "image/characters/robb-stark.png"
+    },
+    {
+        id: 8,
+        name: "sansa stark",
+        family: "stark",
+        imgAddress: "image/characters/sansa-stark.png"
+    }
 ]
 
 
@@ -71,4 +71,6 @@ const characters = [{
 //     {id: 8, name:, imgAddress:,},
 // ]
 
-export {characters};
+export {
+    characters
+};
--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
-import {characters} from "./data-base-justForTest.js";
+import {
+    characters
+} from "./data-base-justForTest.js";
 
 const $ = document;
-const getId = id => $.querySelector(id)
-const getClass = classE => $.getElementById(classE)
+const getId = id => $.getElementById(id)
+const getClass = classE => $.querySelectorAll(classE)
 
 const searchInput = getId('search-input')
 window.addEventListener('load', searchInputPlaceholder)
@@ -14,20 +16,47 @@ function searchInputPlaceholder() {
     }
 }
 
-function pageScroll() {
+function pageScroll(scrollValue) {
     let i = window.scrollY;
     let scrollinterval = setInterval(() => {
+        if (i == scrollValue) {
+            clearInterval(scrollinterval)
+        }
         window.scrollTo(0, i)
         i++;
-    }, 2)
-    setTimeout(() => {
-        clearInterval(scrollinterval, )
-    }, 600)
+    }, 1)
 }
-document.getElementById('scroll-arrow').addEventListener('click', pageScroll)
 
-const charSwiperWrapper = getId('#char-swiper-wrapper')
+getId('scroll-arrow').addEventListener('click', () => pageScroll(window.innerHeight))
 
+const charSwiperWrapper = getId('char-swiper-wrapper')
 characters.forEach(character => {
-    charSwiperWrapper.insertAdjacentHTML('beforeEnd', '<div class="swiper-slide"> <img src="' + character.imgAddress +'" alt="" id="char-profile"> <h1 id="char-name">' + character.name + '</h1> <div id="actions"><i class="fa fa-heart-o"></i> <button id="see-more-about-char">see more</button> </div> </div>');
+    charSwiperWrapper.insertAdjacentHTML('beforeEnd', '<div class="swiper-slide"> <img src="' + character.imgAddress + '" alt="" id="char-profile"> <h1 id="char-name">' + character.name + '</h1> <div id="actions"><i class="fa fa-heart-o"></i> <button id="see-more-about-char">see more</button> </div> </div>');
+});
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+        },
+    },
 });
