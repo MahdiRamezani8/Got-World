af2eddc0eaeb94d347dfa41dcc5108800a128004
mahdi <ramezani86mraz@gmail.com>
2023-06-19 19:17:53 +0330

    vpn connected after 2000 years later and swiper installed completly and extra pics deleted and 8 char profile oic added (png - 512px * 512px)

---
+++ script/data-base-justForTest.js
@@ -0,0 +1,43 @@
+const characters = [{
+    id: 1,
+    name: "john snow",
+    family: "stark"
+},
+{
+    id: 2,
+    name: "tyrion lanister",
+    family: "lanister"
+},
+{
+    id: 3,
+    name: "daenerys targaryen",
+    family: "targaryen"
+},
+{
+    id: 4,
+    name: "jaime lanister",
+    family: "lanister"
+},
+{
+    id: 5,
+    name: "arya stark",
+    family: "stark"
+},
+{
+    id: 6,
+    name: "cersie lanister",
+    family: "lanister"
+},
+{
+    id: 7,
+    name: "robb stark",
+    family: "stark"
+},
+{
+    id: 8,
+    name: "sansa stark",
+    family: "stark"
+}
+]
+
+export {characters};
--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
+import {characters} from "./data-base-justForTest";
+
+console.log(characters);
+
 const $ = document;
 const getId = id => $.getElementById(id)
 const getClass = classE => $.getElementById(classE)
@@ -5,7 +9,8 @@ const getClass = classE => $.getElementById(classE)
 const searchInput = getId('search-input')
 window.addEventListener('load', searchInputPlaceholder)
 window.addEventListener('resize', searchInputPlaceholder)
-function searchInputPlaceholder(){
+
+function searchInputPlaceholder() {
     if (window.innerWidth < 375) {
         searchInput.placeholder = "درباره چی میخوای بدونی؟"
     }
@@ -13,25 +18,14 @@ function searchInputPlaceholder(){
 
 function pageScroll() {
     let i = window.scrollY;
-    let scrollinterval = setInterval(()=>{
-        window.scrollTo(0,i)
+    let scrollinterval = setInterval(() => {
+        window.scrollTo(0, i)
         i++;
-    },2)
+    }, 2)
     setTimeout(() => {
-        clearInterval(scrollinterval,)
-    },600)
+        clearInterval(scrollinterval, )
+    }, 600)
 }
 
 
 document.getElementById('scroll-arrow').addEventListener('click', pageScroll)
-
-const characters = [
-    {id: 1, name: "john snow", family: "stark"},
-    {id: 2, name: "tyrion lanister", family: "lanister"},
-    {id: 3, name: "daenerys targaryen", family: "targaryen"},
-    {id: 4, name: "jaime lanister", family: "lanister"},
-    {id: 5, name: "arya stark", family: "stark"},
-    {id: 6, name: "cersie lanister", family: "lanister"},
-    {id: 7, name: "robb stark", family: "stark"},
-    {id: 8, name: "sansa stark", family: "stark"}
-]
