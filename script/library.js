2eec055c9dc5305dc7f6016c8c49483ac009143b
mahdi <ramezani86mraz@gmail.com>
2023-07-12 20:37:17 +0330

    library page ui improved

--- script/library.js
+++ script/library.js
@@ -1,4 +1,19 @@
-import { $, getClass, getId, getTag } from "./mainScript.js";
+import {
+    $,
+    getClass,
+    getId,
+    getTag
+} from "./mainScript.js";
 
-// const bookCircle = Array.from(getClass('.circle'))
-// window.addEventListener('load', ch)
+function pageScroll(scrollValue) {
+    let i = window.scrollY;
+    const scrollInterval = setInterval(() => {
+        if (i > scrollValue) {
+            clearInterval(scrollInterval);
+        }
+        window.scrollTo(0, i);
+        i += 3;
+    }, 1);
+}
+
+getId('scroll-arrow').addEventListener('click', () => pageScroll(window.innerHeight / 2.7))
