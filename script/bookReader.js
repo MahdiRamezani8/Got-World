0df240d9a788784ddd949b5804f1ec174b1b125d
mahdi <ramezani86mraz@gmail.com>
2023-08-06 21:52:34 +0330

    next an dprev page buttons are responsive, (book reader page)

--- script/bookReader.js
+++ script/bookReader.js
@@ -5,25 +5,15 @@ import {
     getTag
 } from "./mainScript.js";
 
-const books = {
-    AGameOfThoness: {
-        chapters: 5,
-        chapter1Pages: 6,
-        chapter2Pages: 3,
-        chapter3Pages: 7,
-        chapter4Pages: 4,
-        chapter5Pages: 9,
-    },
-    AClashOfKings: {
-        chapters: 3,
-        chapter1Pages: 9,
-        chapter2Pages: 3,
-        chapter3Pages: 12,
-    }
-}
-
 const bookTextesContainer = getTag('main')
 const bookText = getId('book')
+const changeColorInput = getId("change-fontColor");
+const changeBackgroundColorInput = getId("change-background");
+const openSettingModal = getId('open-setting')
+const closeSettingModal = getId('close-setting')
+const settingModal = getId('page-editing-controls-modal')
+const changeTextsFontSize = getId('font-size')
+const changeTextsFont = getId('fonts')
 
 function handleColorChange(event, isBackgroundColor) {
     const colorValue = event.target.value;
@@ -34,31 +24,10 @@ function handleColorChange(event, isBackgroundColor) {
     changeBackgroundColorInput.value = isBackgroundColor ? colorValue : reversedColor;
 }
 
-const changeColorInput = getId("change-fontColor");
-const changeBackgroundColorInput = getId("change-background");
-
 changeBackgroundColorInput.addEventListener("change", event => handleColorChange(event, true));
 changeColorInput.addEventListener("change", event => handleColorChange(event, false));
-
-const changeTextsFont = getId('fonts')
 changeTextsFont.addEventListener('change', e => bookText.style.fontFamily = e.target.value)
-
-
-// font size input (in the top-left of doc)
-const changeTextsFontSize = getId('font-size')
-
-// adding event to font size element
 changeTextsFontSize.addEventListener('change', e => bookText.style.cssText = 'font-size:' + e.target.value + 'px !important')
-
-const paragraph = document.getElementById("book");
-const lineHeight = parseInt(window.getComputedStyle(paragraph).getPropertyValue("line-height"));
-const height = paragraph.getBoundingClientRect().height;
-const numberOfLines = Math.round(height / lineHeight);
-
-const openSettingModal = getId('open-setting')
-const closeSettingModal = getId('close-setting')
-const settingModal = getId('page-editing-controls-modal')
-
 openSettingModal.addEventListener('click', () => settingModal.classList.add('show'))
 closeSettingModal.addEventListener('click', () => settingModal.classList.remove('show'))
 
@@ -74,6 +43,9 @@ function reverseColor(hexColor) {
         invertedR.toString(16).padStart(2, '0') +
         invertedG.toString(16).padStart(2, '0') +
         invertedB.toString(16).padStart(2, '0');
-
     return invertedHexColor;
 }
+
+const lineHeight = parseInt(window.getComputedStyle(bookText).getPropertyValue("line-height"));
+const height = bookText.getBoundingClientRect().height;
+const numberOfLines = Math.round(height / lineHeight);
